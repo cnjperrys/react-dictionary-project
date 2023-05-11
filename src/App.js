862bbda74797b1ae48e08eb4a5d642d069726058
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-05-11 00:51:40 -0400

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo-light2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} classname="img-fluid" alt="cjp logo" />
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;

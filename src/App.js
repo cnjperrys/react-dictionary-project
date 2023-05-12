f1f6213baeab9f4766ff71fa19ff0ea8095d24a5
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-05-12 01:07:16 -0400

    added search form/updated css
    installed Bootstrap
    added Dictionary component -search
    updated css files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo-light2.png';
+import logo from './logo-light.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} classname="img-fluid" alt="cjp logo" />
+        <img src={logo} classname="App-logo-light img-fluid" alt="cjp logo" />
       
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+       <small>Coded by Courtney J. Perry</small> 
+      </footer>
+      </div>
     </div>
   );
 }
